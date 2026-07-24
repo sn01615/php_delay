b2f0ff3e59a71af26a59040a54f55a0fb7df988f
YangLong <168119405@qq.com>
2026-07-24 09:38:42 +0800

    update

--- Tools/PhpDelay.php
+++ Tools/PhpDelay.php
@@ -4,31 +4,9 @@ namespace Tools;
 
 class PhpDelay
 {
-    private static ?self $instance = null;
 
     /** @var callable[] */
     private array $tasks = [];
-
-    private function __construct()
-    {
-    }
-
-    private function __clone()
-    {
-    }
-
-    public function __wakeup()
-    {
-        throw new \RuntimeException("Cannot unserialize singleton.");
-    }
-
-    public static function getInstance(): self
-    {
-        if (self::$instance === null) {
-            self::$instance = new self();
-        }
-        return self::$instance;
-    }
 
     public function push(callable $closure): self
     {
